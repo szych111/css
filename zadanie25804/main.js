f84a0b9a6904b3c1a8d9cc5c98e71c1dbc86089e
Aleksandra Szychlinska <szychlinska@gmail.com>
2020-09-03 15:19:11 +0200

    operator poprawiony

--- zadanie25804/main.js
+++ zadanie25804/main.js
@@ -1,6 +1,6 @@
 function equalsTwenty(num1, num2) {
-    if(num1 === 20 | num2 === 20 | (num1 + num2) <= 20) {
+    if(num1 === 20 || num2 === 20 || (num1 + num2) <= 20) {
         return true;
     }
 }
-console.log(equalsTwenty(5, 15));
+console.log(equalsTwenty(5,20));
