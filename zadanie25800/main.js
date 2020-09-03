8d5267527eb98defa45fd771bb5d9611725fe5da
Aleksandra Szychlinska <szychlinska@gmail.com>
2020-09-03 15:11:06 +0200

    zapakowane w funkcje, funkcja wywolana

--- zadanie25800/main.js
+++ zadanie25800/main.js
@@ -1,7 +1,12 @@
 let maybeTrue = 'Uwielbiam JavaScript';
 let notTrueYet = 'Jestem swietnym programista';
+
+function stringLength(maybeTrue, notTrueYet) {
 if (maybeTrue.length>notTrueYet.length) {
     console.log(maybeTrue);
 } else {
     console.log(notTrueYet);
 };
+};
+
+stringLength(maybeTrue, notTrueYet);
