4920a66e8a7495f22bbf87c34469aa0dceed1212
Aleksandra Szychlinska <szychlinska@gmail.com>
2020-09-03 14:56:09 +0200

    usuniete nawiasy w wywolaniu funkcji

--- zadanie25799/main.js
+++ zadanie25799/main.js
@@ -1,6 +1,6 @@
 let myPar = document.getElementById('opis');
 let myButton = document.querySelector('button');
-myButton.onclick = tresc();
+myButton.onclick = tresc;
 
 function tresc() {
     myPar.textContent = 'Poznaj nasz nowy produkt';
