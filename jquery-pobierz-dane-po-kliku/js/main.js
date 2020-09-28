89619f285a073a9fc21ca48abaabebfc5a36367b
Aleksandra Szychlinska <szychlinska@gmail.com>
2020-09-28 10:16:55 +0200

    warsztat przerobiony

---
+++ jquery-pobierz-dane-po-kliku/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+    $('#get-data').click(function() {
+        
+        /* wariant get
+        $.get('https://akademia108.pl/api/ajax/get-post.php')
+            .done(function(data) {
+                let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+                let pUserId = $('<p></p>').text(`Post ID: ${data.userId}`);
+                let pTitle = $('<p></p>').text(`Post ID: ${data.title}`);
+                let pBody = $('<p></p>').text(`Post ID: ${data.body}`);
+                let hr = $('<hr />');
+
+                let jqBody = $('body'); 
+
+                jqBody.append(pId);
+                jqBody.append(pUserId);
+                jqBody.append(pTitle);
+                jqBody.append(pBody);
+                jqBody.append(hr);
+
+                console.log(pId);
+            })
+            .fail(function(error) {
+                console.error(error);
+            }) */
+
+        // wariant getJSON
+
+        $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
+            .done(function(data) {
+                let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+                let pUserId = $('<p></p>').text(`Post ID: ${data.userId}`);
+                let pTitle = $('<p></p>').text(`Post ID: ${data.title}`);
+                let pBody = $('<p></p>').text(`Post ID: ${data.body}`);
+                let hr = $('<hr />');
+
+                let jqBody = $('body'); 
+
+                jqBody.append(pId);
+                jqBody.append(pUserId);
+                jqBody.append(pTitle);
+                jqBody.append(pBody);
+                jqBody.append(hr);
+
+                console.log(pId);
+            })
+            .fail(function(error) {
+                console.error(error);
+            })
+    })
+        
+})
+
